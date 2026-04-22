852771d5a5a8f1034b8a81064cd64988c8f81e56
ivanovk0440-dotcom <ivanovk0440@gmail.com>
2026-04-22 21:01:14 +0300

    TEST

--- provisioning.sh
+++ provisioning.sh
@@ -78,7 +78,7 @@ echo "=== Installing dependencies ==="
 
 echo "=== Dependencies installed ==="
 
-# Создаём worker.py на порту 8288 (ИСПРАВЛЕННАЯ ВЕРСИЯ)
+# Создаём worker.py на порту 8288
 cat > /workspace/ComfyUI/worker.py << 'EOF'
 import json, base64, time, os, requests
 from flask import Flask, request, jsonify
@@ -100,15 +100,7 @@ def generate():
         with open(img_path, 'wb') as f:
             f.write(base64.b64decode(img_b64))
         
-        # === ИСПРАВЛЕНИЕ №1: Правильный путь к полю image ===
-        if '148' in workflow:
-            workflow['148']['inputs']['image'] = 'temp.jpg'
-        else:
-            # Fallback: ищем любую ноду LoadImage
-            for node_id, node in workflow.items():
-                if node.get('class_type') == 'LoadImage':
-                    node['inputs']['image'] = 'temp.jpg'
-                    break
+        workflow['148']['widgets_values'][0] = 'temp.jpg'
         
         for i in range(30):
             try:
@@ -133,15 +125,8 @@ def generate():
                     outputs = data[prompt_id]['outputs']
                     for node_id, node_output in outputs.items():
                         if 'videos' in node_output:
-                            # === ИСПРАВЛЕНИЕ №2: Правильное формирование URL с subfolder и type ===
-                            video_info = node_output['videos'][0]
-                            video_filename = video_info['filename']
-                            subfolder = video_info.get('subfolder', '')
-                            video_type = video_info.get('type', 'output')
-                            
-                            # Формируем корректный URL для скачивания
-                            url = f"http://localhost:18188/view?filename={video_filename}&subfolder={subfolder}&type={video_type}"
-                            return jsonify({'video_url': url})
+                            video_filename = node_output['videos'][0]['filename']
+                            return jsonify({'video_url': f'http://localhost:18188/view?filename={video_filename}'})
             except:
                 pass
             time.sleep(2)
